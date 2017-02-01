8246d04e930ecc7d595e618f61d5cf33c88d27cd
Tomek Wałkuski <ja@jestem.tw>
2017-02-01 21:33:41 +0100

    Fix calculating code coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,16 @@
 require "simplecov"
 
+SimpleCov.start do
+  add_group "Library", "lib"
+  add_filter "/spec/"
+end
+
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "hucpa"
 
 if ENV["CIRCLE_ARTIFACTS"]
   SimpleCov.coverage_dir(File.join(ENV["CIRCLE_ARTIFACTS"], "coverage"))
 end
-
-SimpleCov.start
 
 RSpec.configure do |config|
   config.order = :random
